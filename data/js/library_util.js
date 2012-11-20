42b7d281165e520077bd9ebdbf2a7c6576c9cc92
Christopher Nabors <PillarDD@Gmail.com>
2012-11-19 19:55:44 -0500

    Still troubles with the color scale

--- data/js/library_util.js
+++ data/js/library_util.js
@@ -7,26 +7,27 @@
 /*------------------------------------------------------*\
  * Globals                                              *
 \*------------------------------------------------------*/
-var margin = {top: 80, right: 0, bottom: 10, left: 80},
-    width = 720,
-    height = 720;
-
-var x = d3.scale.ordinal().rangeBands([0, width]),
-    z = d3.scale.linear().domain([0, 4]).clamp(true),
-    c = d3.scale.category10().domain(d3.range(10));
-
-var svg = d3.select('#chart_area')
-            .append('svg')
-            .attr('width', (width + margin.left + margin.right))
-            .attr('height', (height + margin.top + margin.bottom))
-            .style('margin-left', (-margin.left + 'px'))
-                .append('g')
-                .attr('transform', 'translate(' + margin.left + ',' + margin.top + ")");
 
 /*------------------------------------------------------*\
  * Functions                                            *
 \*------------------------------------------------------*/
 function initialize(){
+    var margin = {top: 80, right: 0, bottom: 10, left: 80},
+        width = 720,
+        height = 720;
+
+    var x = d3.scale.ordinal().rangeBands([0, width]),
+        z = d3.scale.linear().domain([0, 4]).clamp(true),
+        c = d3.scale.category10().domain(d3.range(10));
+
+    var svg = d3.select('#chart_area')
+                .append('svg')
+                .attr('width', (width + margin.left + margin.right))
+                .attr('height', (height + margin.top + margin.bottom))
+                .style('margin-left', (-margin.left + 'px'))
+                    .append('g')
+                    .attr('transform', 'translate(' + margin.left + ',' + margin.top + ")");
+
     // AJAX Loading of JSON
     d3.json('/data/json/alachua.json', function(libraries) {
         var matrix = [],
